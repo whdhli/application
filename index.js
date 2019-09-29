85d3d512caca4ab60b45cf3e017127bf3f82547d
hda <isolatingbeats@gmail.com>
2019-09-29 22:05:14 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+var app = Express();
+
+
+var port = process.env.PORT || 8080;
+
+// Send message for default URL
+app.get('/', (req, res) => res.send('Hello. add in in the link ?busNumber=yourchoice%startingBusStopCode=yourchoice &endingBusStopCOde=yourchoice&direction=1'));
+    
+    
+     DistanceBetweenStops = (busNumber, startingBusStopCode, endingBusStopCode, direction) => {
+         let rawdata = fs.readFileSync('database/routes.json');
+         let routes = JSON.parse(rawdata);
+     
+         const findDist = (busNumber, busStop, direction) => {
+             for(i=0; i <routes.length; i++) {
+                 if(busNumber == routes[i].ServiceNo && busStop == routes[i].BusStopCode && direction == routes[i].Direction) {
+                     return routes[i].Distance; 
+                 }
+             }
+         }
+     
+         let DistBus1 = findDist(busNumber, startingBusStopCode, direction);
+         let DistBus2 = findDist(busNumber, endingBusStopCode, direction);
+     
+         return  DistBus2 - DistBus1;
+     
+     }
+     
+     
+     var final = findDist(req.query.busNumber, req.query.startingBusStopCode, req.query.endingBusStopCode, req.query.direction); 
+     
+     
+     res.send(`The distance between the stops is ${finalDist}`); 
+ })
+
+app.listen(port, function () {
+    console.log("busrouting at port " + port);
+});
